3acd1b7ee90d9493033124c49605d0cdfa5fc73a
JReaves10 <J_C_Reaves@hotmail.com>
2025-02-05 22:28:11 -0500

    Update EVChargingTrends.py

--- EVChargingTrends.py
+++ EVChargingTrends.py
@@ -2,10 +2,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+# read in data
 private_df = pd.read_csv('private_ev_charging.csv')
 public_df = pd.read_csv('public_ev_charging.csv')
 sales_df = pd.read_csv('ev_sales.csv')
 
+# merge tables to keep rows with complete information
 merged_df = private_df.merge(public_df, how='outer', indicator=True)
 merged_df = merged_df[merged_df['_merge'] == 'both']
 merged_df.drop(columns=['_merge'], inplace=True)
@@ -13,12 +15,16 @@ merged_df.drop(columns=['_merge'], inplace=True)
 merged_df = merged_df.merge(sales_df, how='left', on='year')
 merged_df.groupby('year')['sales'].sum()
 
+# number of vehicles sold in 2018
 ev_sales_2018 = 361315
 
+# trends between public ports, private ports and sales
 fig, ax = plt.subplots()
 sns.lineplot(data=merged_df, x='year', y='private_ports', label='Private Ports')
 sns.lineplot(data=merged_df, x='year', y='public_ports', label='Public Ports')
-sns.lineplot(data=merged_df, x='year', y='sales', label='Sales')
-plt.show()
+sns.lineplot(data=merged_df, x='year', y='sales', label='Total Sales', linestyle='--')
 
-trend = 'same'
+ax.set_title('EV Ports and Sales Over Time')
+ax.set(xlabel='Year', ylabel='Count')
+
+plt.show()
